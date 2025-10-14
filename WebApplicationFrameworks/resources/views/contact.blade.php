f15e53d8bd345b41541fd9960ac67fbe9bafd65a
Sebastiaan Nijs <47386986+Sebastiaan010@users.noreply.github.com>
2025-10-14 13:05:56 +0200

    Lesson 2 & 3
    Lesson 2 & 3 of Learning laravel in 30 days

---
+++ WebApplicationFrameworks/resources/views/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Contact page</title>
+</head>
+
+<body>
+
+    <h1>Hello from the Contact page</h1>
+    <p>My first paragraph on the Contact page</p>
+
+</body>
+
+</html>
---
+++ WebApplicationFrameworks/resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Home page</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Hello from the home page</h1>
+    <p>My first paragraph on the home page</p>
+
+</body>
+
+</html>
