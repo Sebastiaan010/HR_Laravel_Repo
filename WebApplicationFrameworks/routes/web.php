7b260815c5c14527e287ba9e86c47e8bcd9dd225
Sebastiaan Nijs <47386986+Sebastiaan010@users.noreply.github.com>
2025-10-16 13:16:00 +0200

    Small changes
    Small changes

--- WebApplicationFrameworks/routes/web.php
+++ WebApplicationFrameworks/routes/web.php
@@ -4,7 +4,16 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
 
 Route::get('/dashboard', function () {
